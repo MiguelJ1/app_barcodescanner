865a1a76b73b26f370b1d202bf0944688d7ccba6
miguel <miguel.jimenez@usantotomas.edu.co>
2022-03-22 01:27:53 -0500

    add code in MainActivity.java

--- app/src/main/java/com/example/barcodecodescannerandroid_2/MainActivity.java
+++ app/src/main/java/com/example/barcodecodescannerandroid_2/MainActivity.java
@@ -1,20 +1,64 @@
 package com.example.barcodecodescannerandroid_2;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.barcodecodescannerandroid_2.BarcodeScanner.BarcodeScanner;
+import com.example.barcodecodescannerandroid_2.BarcodeScanner.Image;
+import com.example.barcodecodescannerandroid_2.dao.IImagesDao;
+import com.example.barcodecodescannerandroid_2.dao.ImagesDaoImp;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private IImagesDao imagesDao;
+
+    private TextView tvMessage;
+    private Button btnScan;
+    private Button btnLoadImage;
+    private ImageView ivImages;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvMessage = (TextView) findViewById(R.id.tv_message);
+        btnScan = (Button) findViewById(R.id.btn_scan);
+        btnLoadImage = (Button) findViewById(R.id.btn_load_img);
+        ivImages = (ImageView) findViewById(R.id.iv_image);
+
+        imagesDao = new ImagesDaoImp();
+        ivImages.setImageBitmap(imagesDao.getNextBitmapImage());
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void scanImage(View view) {
+        Image image = new Image(((BitmapDrawable) ivImages.getDrawable()).getBitmap());
+
+        Date start = new Date();
+
+        BarcodeScanner barcodeScanner = new BarcodeScanner();
+        String message = barcodeScanner.read(image);
+
+        long duration = new Date().getTime() - start.getTime();
+        tvMessage.setText(String.format("%s\n%s",
+                "Duration = " + duration,
+                "Message = " + message
+        ));
     }
 
     public void loadImage(View view) {
+        ivImages.setImageBitmap(imagesDao.getNextBitmapImage());
     }
 }
