051141b570a01389d35dc6c4c144708e450c3caa
miguel <miguel.jimenez@usantotomas.edu.co>
2022-03-22 01:27:52 -0500

    add view in activity_main.xml and MainActivity.java

--- app/src/main/java/com/example/barcodecodescannerandroid_2/MainActivity.java
+++ app/src/main/java/com/example/barcodecodescannerandroid_2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.barcodecodescannerandroid_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scanImage(View view) {
+    }
+
+    public void loadImage(View view) {
     }
 }
